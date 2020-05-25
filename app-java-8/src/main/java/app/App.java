f96bac52bb89850ba179948ac0aeb0752b030bd4
Pierre Cazajous <pierre.cazajous@gmail.com>
2020-05-25 23:12:16 +0200

    Initial commit

---
+++ app-java-8/src/main/java/app/App.java
@@ -0,0 +1,13 @@
+package app;
+
+import lib.Library;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting() + new Library().someLibraryMethod());
+    }
+}
---
+++ lib-java-14/src/main/java/lib/Library.java
@@ -0,0 +1,7 @@
+package lib;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
